6a193e81ba3510cccafb4610ddfdd4192dd8862e
ryan <ryan-polley@uiowa.edu>
2017-04-22 14:56:53 -0500

    refactored probability distributions into thier own class

--- distributions.py
+++ distributions.py
@@ -1,7 +1,40 @@
 import random
 from functools import reduce
 def dice(sides_i):
-	return {n: 1/sides_i for n in range(1,sides_i+1)}
+	return distr({n: 1/sides_i for n in range(1,sides_i+1)})
+
+class distr(dict):
+	def __init__(self, d):
+		dict.__init__(self, d)
+
+	def __gt__(self, other):
+		if(type(other)==int):
+			return bool_scalar(lambda x, y: x>y, self, other)
+		else:
+			return bool_distr(lambda x, y: x>y, self, other)
+
+
+
+def bool_scalar(funct, dist, scal):
+	result = {0: 0, 1: 0}
+	for value, prob in dist.items():
+		if(funct(value,scal)):
+			result[1]+=prob
+		else:
+			result[0]+=prob
+	return distr(result)
+
+def bool_distr(funct, dist1, dist2):
+	result = {0: 0, 1: 0}
+	for value1, prob1 in dist1.items():
+		for value2, prob2 in dist2.items():
+			if(funct(value1, value2)):
+				result[1]+=prob1*prob2
+			else:
+				result[0]+=prob1*prob2
+	return distr(result)
+
+
 
 
 def roll(dice):
@@ -10,28 +43,30 @@ def roll(dice):
 		r-=prob
 		if r<=0:
 			return value
+	else:
+		return roll(dice) #we fucked up because of floating point arithmatic somewhere
 
 def do_oper1(funct, arg_d):
-	return {funct(n): prob for n, prob in arg_d.items()}
+	return distr({funct(n): prob for n, prob in arg_d.items()})
 
 def do_oper2(funct, arg1_d, arg2_d):
-	distr = {funct(val1, val2): 0 for val1 in arg1_d.keys() for val2 in arg2_d.keys()}
+	dist = {funct(val1, val2): 0 for val1 in arg1_d.keys() for val2 in arg2_d.keys()}
 	for val1, prob1 in arg1_d.items():
 		for val2, prob2 in arg2_d.items():
-			distr[funct(val1,val2)]+=prob1*prob2
-	return distr
+			dist[funct(val1,val2)]+=prob1*prob2
+	return distr(dist)
 
 def iter_oper(funct, args_d):
 	return reduce((lambda arg1_d, arg2_d: do_oper2(funct, arg1_d,arg2_d)), args_d)
 
-def scalar_add(distr, scal):
-	return do_oper1(lambda x: x+scal, distr)
+def scalar_add(dist, scal):
+	return do_oper1(lambda x: x+scal, dist)
 
-def scalar_mult(distr, scal):
-	return do_oper1(lambda x: x*scal, distr)
+def scalar_mult(dist, scal):
+	return do_oper1(lambda x: x*scal, dist)
 
-def add(distr1, distr2):
-	return do_oper2(lambda x, y: x+y, distr1, distr2)
+def add_distr(dist1, dist2):
+	return do_oper2(lambda x, y: x+y, dist1, dist2)
 
-def iter_add(distr, scal):
-	return iter_oper(lambda x,y: x+y, [distr]*scal)
+def iter_add_distr(dist, scal):
+	return iter_oper(lambda x,y: x+y, [dist]*scal)
