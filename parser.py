9c6028b6ffbe740278a7a5f70decfa5fb04902ee
ryan <ryan-polley@uiowa.edu>
2017-04-24 10:32:30 -0500

    added basic expression parsing

--- parser.py
+++ parser.py
@@ -1,1 +1,9 @@
-import distributions
+from lib.pyparsing import *
+integer = Word(nums)
+variable = Word(srange("[A-Z]"))#variable names must be all caps
+dice_func = infixNotation(integer | variable, 
+	[
+		("d", 2, opAssoc.LEFT),
+		(oneOf("* /"), 2, opAssoc.LEFT),
+		(oneOf("+ -"), 2, opAssoc.LEFT),
+	])
