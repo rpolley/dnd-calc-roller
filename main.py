7730347037e5cd5adfb4f68347ad50c2908acfcd
ryan <ryan-polley@uiowa.edu>
2017-04-24 14:00:21 -0500

    bugfix

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from distributions import *
 import parser
 import fileinput
 
-for line in fileinput.input()
-	parsed_cmd = parser.expr.parseString(line)
-	print(evaluate(parsed_cmd))
+__log__ = False
 
 def evaluate(cmd):
-	if len(cmd)==1:
-		return cmd[0]
-	elif type(cmd)==str:
+	if(__log__): print(list(cmd))
+	if type(cmd)==str:
 		return int(cmd)
+	elif len(list(cmd))==1:
+		return evaluate(cmd[0])
 	elif type(cmd)==int:
 		return cmd
-	elif len(cmd)==3
+	elif len(list(cmd))==3:
 		cmd[0] = evaluate(cmd[0])
 		cmd[2] = evaluate(cmd[2])
 		if cmd[1] == 'd':
@@ -36,11 +35,15 @@ def typed_call(funct, arg1, arg2):
 		arg2 = sum_distr(arg2)
 
 	#then guess the type and apply the correct function
-	if type(arg1)==int&&type(arg2)==int:
+	if type(arg1)==int and type(arg2)==int:
 		return funct(arg1,arg2)
-	elif type(arg1)==distr&&type(arg2)==int:
+	elif type(arg1)==distr and type(arg2)==int:
 		return do_oper1(lambda num: funct(num, arg2), arg1)
-	elif type(arg1)==int&&type(arg2)==distr:
+	elif type(arg1)==int and type(arg2)==distr:
 		return do_oper1(lambda num: funct(num, arg2), arg1)
-	elif type(arg1)==distr&&type(arg2)==distr:
+	elif type(arg1)==distr and type(arg2)==distr:
 		return do_oper2(funct, arg1, arg2)
+
+for line in fileinput.input():
+	parsed_cmd = parser.expr.parseString(line)
+	print(evaluate(parsed_cmd))
