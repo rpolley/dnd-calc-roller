c5733a3b0214820400899e0aff0504be25d19dfa
ryan <ryan-polley@uiowa.edu>
2017-04-25 16:50:05 -0500

    added variables

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ import fileinput
 
 __log__ = False
 
+
 def evaluate(cmd):
-	if(__log__): print(cmd)
-	if type(cmd)==str:
+	if(__log__): 
+		print(cmd)
+		print(variables)
+	if type(cmd)==str and str.isnumeric(cmd):
 		return int(cmd)
-	elif len(cmd)==1:
-		return evaluate(cmd[0])
+	elif type(cmd)==str and cmd in variables.keys():
+		return variables[cmd]
+	elif type(cmd)==str:
+		return cmd
 	elif type(cmd)==int:
 		return cmd
+	elif len(cmd)==1:
+		return evaluate(cmd[0])
 	elif len(cmd)==2:
+		cmd[1] = evaluate(cmd[1])
 		if cmd[0]=="roll":				
-			return roll(evaluate(cmd[1]))
+			return roll(cmd[1])
+		elif cmd[0]=="d":
+			return typed_call(lambda n, m: n*[dice(m)], 1, cmd[1])
 	elif len(cmd)==3:
+		if cmd[1] == "=":
+			variables[cmd[0]]=evaluate(cmd[2])
 		cmd[0] = evaluate(cmd[0])
 		cmd[2] = evaluate(cmd[2])
 		if cmd[1] == 'd':
@@ -28,6 +40,7 @@ def evaluate(cmd):
 			return typed_call(lambda n, m: n+m, cmd[0], cmd[2])
 		elif cmd[1] == "-":
 			return typed_call(lambda n, m: n-m, cmd[0], cmd[2])
+		
 	else:
 		return cmd
 		
@@ -48,6 +61,9 @@ def typed_call(funct, arg1, arg2):
 		return do_oper1(lambda num: funct(num, arg2), arg1)
 	elif type(arg1)==distr and type(arg2)==distr:
 		return do_oper2(funct, arg1, arg2)
+
+
+variables = {}
 
 for line in fileinput.input():
 	parsed_cmd = parser.expr.parseString(line)
--- parser.py
+++ parser.py
@@ -1,10 +1,13 @@
 from lib.pyparsing import *
 integer = Word(nums)
-dice_expr = infixNotation(integer, 
+variable = Word(srange("[A-Z]")+nums)
+dice_expr = infixNotation(integer | variable, 
 	[
+		("d", 1, opAssoc.RIGHT),
 		("d", 2, opAssoc.LEFT),
 		(oneOf("* /"), 2, opAssoc.LEFT),
 		(oneOf("+ -"), 2, opAssoc.LEFT),
 		(Keyword("roll"), 1, opAssoc.RIGHT)
 	])
-expr = dice_expr
+variable_definition = variable + "=" + (dice_expr|variable)
+expr = dice_expr ^ variable_definition
