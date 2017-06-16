0434bcb79fd7aa1e4b6d66c5a4ad4add9c44f6b8
ryan <ryan-polley@uiowa.edu>
2017-06-16 17:48:48 -0500

    added several classes to represent state

--- effects.py
+++ effects.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 class Effect(): 
 	"""
 	check_trigger:
@@ -9,3 +10,23 @@ class Effect():
 	def __init__(self, check_trigger,do_effect):
 		self.check_trigger = check_trigger
 		self.do_effect = do_effect
+
+State = namedtuple("State", [
+		"initiative",	#current initiative count
+		"phase",		#what phase of the turn it is
+ 		"chars"			#the current characters involved
+	])
+Faction = namedtuple("Faction",[
+		"name",
+		"enemies"
+	])
+Character = namedtuple("Character",[
+		"faction",
+		"position",
+		"health",
+		"effects"
+	])
+Position = namedtuple("Position",[
+		"x",
+		"y"
+	])
